dac3776b4827a73a3abd4ee679908f898e776d20
florian <florian.vialet@gmail.com>
2023-06-13 13:58:17 +0200

    nav

--- src/Controller/NavAdminController.php
+++ src/Controller/NavAdminController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class NavController extends AbstractController
+class NavAdminController extends AbstractController
 {
-    #[Route('/', name: 'app_nav')]
+    #[Route('/admin', name: 'app_nav_admin')]
     public function index(): Response
     {
-        return $this->render('nav/index.html.twig', [
-            'controller_name' => 'NavController',
+        return $this->render('nav_admin/index.html.twig', [
+            'controller_name' => 'NavAdminController',
         ]);
     }
 }
