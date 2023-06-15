8af7eb9171cb169e8dd7a967aa66b51308ed3247
Madiha belouadah <117091050+madiha-bel@users.noreply.github.com>
2023-06-15 11:55:54 +0200

    add crud

---
+++ src/Form/DossierType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Dossier;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DossierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('formation_initiale')
+            ->add('experience_pro')
+            ->add('candidat')
+            ->add('them_forma_question')
+            ->add('promo_formation')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Dossier::class,
+        ]);
+    }
+}
