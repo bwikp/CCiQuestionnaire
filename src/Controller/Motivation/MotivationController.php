27a8548b0319b27ac0172027b6feba5c9eaad9ad
florian <florian.vialet@gmail.com>
2023-06-19 16:42:47 +0200

    yes

--- src/Controller/Motivation/MotivationController.php
+++ src/Controller/Motivation/MotivationController.php
@@ -22,13 +22,5 @@ class MotivationController extends AbstractController
     }
 
 
-    #[Route('/{id}', name: 'app_motivation_delete', methods: ['POST'])]
-    public function delete(Request $request, Motivation $motivation, MotivationRepository $motivationRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$motivation->getId(), $request->request->get('_token'))) {
-            $motivationRepository->remove($motivation, true);
-        }
-
-        return $this->redirectToRoute('app_motivation_index', [], Response::HTTP_SEE_OTHER);
-    }
+    
 }
