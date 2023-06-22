006859be835f1980b704cf90fe8b3c79abae2c7f
Bwikp <zilpa.michel@gmail.com>
2023-06-22 16:56:14 +0200

    tout est  fait

--- src/Form/UsersCCIType.php
+++ src/Form/UsersCCIType.php
@@ -4,16 +4,25 @@ namespace App\Form;
 
 use App\Entity\UsersCCI;
 use Symfony\Component\Form\AbstractType;
+use App\Repository\UsersCCIRepository;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UsersCCIType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $roles): void
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles',ChoiceType::class,[
+                'required' => true,
+                'multiple' => true,
+                'expanded' => true,
+                'choices' => [
+                    'ADMIN' => 'ROLE_ADMIN'
+                ]
+            ])
             ->add('password')
         ;
     }
